1d66386fb27243883f8f36762e61e8c72b0d72a7
Matthieu Barba <mbarba@ebi.ac.uk>
2018-10-24 16:08:14 +0100

    Insert splices in sqlite db

--- lib/splices_extract.py
+++ lib/splices_extract.py
@@ -2,21 +2,28 @@
 
 import argparse
 import logging
+import os.path
 
 # To parse a BAM file
 import HTSeq
+import sqlite3
 
 
 class Splice():
     """Splice junction found in a read"""
 
-    def __init__(self, chrom, start, end, strand, left_flank=1, right_flank=1):
+    sql_fields = ('chrom', 'start', 'end', 'strand',
+                  'left_flank', 'right_flank', 'coverage',)
+
+    def __init__(self, chrom, start, end, strand,
+                 left_flank=1, right_flank=1, coverage=1):
         self.chrom = chrom
         self.start = start
         self.end = end
         self.strand = strand
         self.left_flank = left_flank
         self.right_flank = right_flank
+        self.coverage = coverage
 
     def __str__(self):
         return "%s:%d-%d (%s) [%d-%d]\n" % (
@@ -64,6 +71,12 @@ class Splice():
 
         return splices
 
+    def sql_values(self):
+        return [getattr(self, f) for f in self.sql_fields]
+
+    def sql_placeholder(self):
+        return ",".join(['?' for f in self.sql_fields])
+
 
 def extract_splices(bam_input, sqlite_output):
     """Extract splices from a bam file, and write them in an SQLite db file"""
@@ -85,15 +98,13 @@ def extract_splices(bam_input, sqlite_output):
             # New chromosome? Store it!
             splice_chrom = new_splices[0].chrom
             if cur_chrom != splice_chrom:
-                logging.info("Writing to " + sqlite_output)
+                logging.debug("From %s to %s: Writing to %s" % (
+                    cur_chrom, splice_chrom, sqlite_output))
                 store_splices(cur_splices, sqlite_output)
                 cur_splices = []
+                cur_chrom = splice_chrom
 
             cur_splices = cur_splices + new_splices
-
-            for s in new_splices:
-                count_splices += 1
-                logging.info("Splice: " + str(s))
 
         if count_splices >= 10:
             break
@@ -104,9 +115,42 @@ def extract_splices(bam_input, sqlite_output):
 
 
 def store_splices(splices, output):
-    with open(output, 'a') as out:
-        for splice in splices:
-            out.write(str(splice))
+
+    # Init sqlite db if new
+    init_sqlite_db(output)
+
+    conn = sqlite3.connect(output)
+    c = conn.cursor()
+
+    for splice in splices:
+        values = splice.sql_values()
+        fields = ",".join(splice.sql_fields)
+        placeholder = splice.sql_placeholder()
+        sql = "INSERT INTO splices("+fields+") VALUES("+placeholder+")"
+        logging.debug(sql)
+        logging.debug(values)
+        c.execute(sql, values)
+    conn.commit()
+    conn.close()
+
+
+def init_sqlite_db(output):
+    if os.path.isfile(output):
+        os.remove(output)
+
+    conn = sqlite3.connect(output)
+    c = conn.cursor()
+
+    c.execute('''CREATE TABLE splices (
+                chrom text,
+                start int,
+                end int,
+                strand text,
+                left_flank int,
+                right_flank int,
+                coverage int
+              )''')
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
