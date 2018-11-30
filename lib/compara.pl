2f2c0388b7ff764d490800aa7e5ce129e61a9d57
Matthieu Barba <mbarba@ebi.ac.uk>
2018-11-30 13:00:54 +0000

    Fix and improve compara script

--- lib/compara.pl
+++ lib/compara.pl
@@ -20,21 +20,36 @@ my %opt = %{ opt_check() };
 
 Bio::EnsEMBL::Registry->load_all($opt{registry});
 
+my ($db1, $db2) = ($opt{db1}, $opt{db2});
+if ($opt{db_dir}) {
+  $db1 = "$opt{db_dir}/$opt{species1}.sqlite" if not $db1;
+  $db2 = "$opt{db_dir}/$opt{species2}.sqlite" if not $db2;
+}
+$logger->info("Use db1 = $db1");
+$logger->info("Use db2 = $db2");
+
+my ($lengths1, $lengths2);
+
+if ($opt{lengths_dir}) {
+  $lengths1 = "$opt{lengths_dir}/$opt{species1}.txt";
+  $lengths2 = "$opt{lengths_dir}/$opt{species2}.txt";
+}
+
 # Get the list of genes covered in the db of interest
-my $genes1 = get_genes_from_db($opt{db1});
-my $genes2 = get_genes_from_db($opt{db2});
+my $genes1 = get_genes_from_db($db1);
+my $genes2 = get_genes_from_db($db2);
 $logger->info(sprintf("%s: %d genes", $opt{species1}, scalar @$genes1));
 $logger->info(sprintf("%s: %d genes", $opt{species2}, scalar @$genes2));
 
-$genes1 = add_lengths($genes1, $opt{species1});
-$genes2 = add_lengths($genes2, $opt{species2});
+$genes1 = add_lengths($genes1, $opt{species1}, $lengths1);
+$genes2 = add_lengths($genes2, $opt{species2}, $lengths2);
 
 # Get the correspondence via compara
 my $match = get_compara($genes1, $opt{species2});
+$logger->info("Matched genes: " . scalar(keys %$match));
 
 # Merge the genes and their orthologs
 my $merged = merge_matches($genes1, $genes2, $match);
-$logger->info("Matched genes: " . scalar(@$merged));
 
 # Print out the result to stdout
 print_match($merged);
@@ -58,33 +73,65 @@ sub get_genes_from_db {
 }
 
 sub add_lengths {
-  my ($genes, $species) = @_;
-  my $tla = Bio::EnsEMBL::Registry->get_adaptor($species, 'core', 'translation');
+  my ($genes, $species, $length_file) = @_;
 
-  $logger->info("Add lengths for $species");
-  
-  my %lengths;
-  my $n = 0;
-  for my $tl (@{ $tla->fetch_all() }) {
-    $n++; $logger->debug("$n translations...") if $n % 1000 == 0;
-    my $length = $tl->length();
-    my $gene_id = $tl->stable_id;
-    $gene_id =~ s/-P[A-Z]+$//;
-    if (not exists $lengths{$gene_id} or $lengths{$gene_id} < $length) {
-      $lengths{$gene_id} = $length;
+  my $lengths = {};
+  if ($length_file and -s $length_file) {
+    $lengths = load_lengths_from_file($length_file);
+  } else {
+    my $tla = Bio::EnsEMBL::Registry->get_adaptor($species, 'core', 'translation');
+
+    $logger->info("Add lengths for $species");
+    
+    my $n = 0;
+    for my $tl (@{ $tla->fetch_all() }) {
+      $n++; $logger->debug("$n translations...") if $n % 1000 == 0;
+      my $length = $tl->length();
+      my $gene_id = $tl->stable_id;
+      $gene_id =~ s/-P[A-Z]+$//;
+      if (not exists $lengths->{$gene_id} or $lengths->{$gene_id} < $length) {
+        $lengths->{$gene_id} = $length;
+      }
     }
   }
 
   $logger->debug("Transfer lengths");
   for my $gene (@$genes) {
-    $gene->{length} = $lengths{ $gene->{gene} };
+    $gene->{length} = $lengths->{ $gene->{gene} };
   }
+  if ($length_file) {
+    store_lengths_in_file($lengths, $length_file);
+  }
+
   return $genes;
+}
+
+sub store_lengths_in_file {
+  my ($lengths, $file) = @_;
+
+  open my $lout, ">", $file;
+  for my $id (keys %$lengths) {
+    say $lout "$id\t$lengths->{$id}";
+  }
+  close $lout;
+}
+
+sub load_lengths_from_file {
+  my ($file) = @_;
+
+  my %lengths;
+  open my $lout, "<", $file;
+  while (my $line = readline $lout) {
+    chomp $line;
+    my ($id, $length) = split /\t/, $line;
+    $lengths{$id} = $length;
+  }
+  close $lout;
+  return \%lengths;
 }
 
 sub get_compara {
   my ($genes, $species2) = @_;
-
 
   # Get the genomedb for species2
   my $genomedba = Bio::EnsEMBL::Registry->get_adaptor('vb', 'compara', 'GenomeDB');
@@ -124,16 +171,18 @@ sub merge_matches {
   my %genes2_hash = map { $_->{gene} => $_ } @$genes2;
 
   my @merged;
-  for my $gene1 (@$genes1) {
+  for my $gene1 (sort { $a->{gene} cmp $b->{gene} } @$genes1) {
+    next if not $gene1->{length};
     my $matched = $matches->{$gene1->{gene}};
+    my $did_match = 0;
     for my $match (@$matched) {
       $match =~ s/-P[A-Z]+$//;
       my $gene2 = $genes2_hash{$match};
-      if (defined $gene2) {
-        push @merged, merged($gene1, $gene2);
-      } else {
-        $logger->warn("No match: $gene1->{gene} vs $match");
-      }
+      push @merged, merged($gene1, $gene2);
+      $did_match = 1;
+    }
+    if (not $did_match) {
+      push @merged, merged($gene1);
     }
   }
   
@@ -143,8 +192,10 @@ sub merge_matches {
 sub merged {
   my ($g1, $g2) = @_;
   
-  return $g1->{gene} if not $g2;
   my $complete1 = $g1->{introns} > 0 ? $g1->{covered_introns}/$g1->{introns} : 1;
+  if (not $g2) {
+    return join("\t", $g1->{gene}, $g1->{length}, $g1->{introns}, $g1->{covered_introns}, $complete1);
+  }
   my $complete2 = $g2->{introns} > 0 ? $g2->{covered_introns}/$g2->{introns} : 2;
   
   my $support = compared($g1, $g2);
@@ -247,8 +298,8 @@ sub print_match {
   my ($merged) = @_;
 
   my @header = qw(
-    #sp1 len1 intr1 cov1 cov1/intr1
-    #sp2 len2 intr2 cov2 cov2/intr2
+    #sp1 length1 introns1 covered_introns1 cov1/intr1
+    sp2 length2 introns2 covered_introns2 cov2/intr2
     support
     introns_diff
     total_diff
@@ -272,11 +323,15 @@ sub usage {
 
     --db1 <path>      : db from which the genes of interest are used
     --db2 <path>      : db compared
+    OR
+    --db_dir <path>   : dir where dbs reside, named with the species_name + .sqlite
 
     --species1 <str>  : species for db1
     --species2 <str>  : species for db2
 
     --registry <path> : path to the registry including compara and the species core dbs
+
+    --lengths_dir <path> : dir where lengths of translations are stored (created if they do not exist)
 
     
     --help            : show this help message
@@ -290,6 +345,8 @@ EOF
 sub opt_check {
   my %opt = ();
   GetOptions(\%opt,
+    "db_dir=s",
+    "lengths_dir=s",
     "db1=s",
     "db2=s",
     "species1=s",
@@ -302,9 +359,9 @@ sub opt_check {
 
   usage()                if $opt{help};
 
-  usage("db1 and db2 needed") if not $opt{db1} or not $opt{db2};
-  usage("species1 and species2 needed") if not $opt{species1} or not $opt{species2};
-  usage("Registry file needed") if not $opt{registry};
+  usage("species1 and species2 needed") unless $opt{species1} and $opt{species2};
+  usage("db1 and db2, or db_dir needed") unless ($opt{db1} and $opt{db2}) or $opt{db_dir};
+  usage("Registry file needed") unless $opt{registry};
 
   Log::Log4perl->easy_init($INFO) if $opt{verbose};
   Log::Log4perl->easy_init($DEBUG) if $opt{debug};
