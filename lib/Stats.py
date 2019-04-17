0d100fa0eec6edcc1e9b429ce432c5494453ef38
Matthieu Barba <mbarba@ebi.ac.uk>
2019-04-17 11:01:46 +0100

    Simpler list of stats instead of dict

--- lib/Stats.py
+++ lib/Stats.py
@@ -10,46 +10,19 @@ from glob import glob
 import os.path
 
 class Stats(eHive.BaseRunnable):
-    queries = {
-            'genes':
-                "SELECT count(*) FROM genes",
-            'genes_without_intron':
-                "SELECT count(*) FROM genes WHERE introns=0",
-            'genes_completely_covered':
-                "SELECT count(*) FROM genes WHERE introns=covered_introns AND introns > 0",
-            'genes_completely_covered_10':
-                "SELECT count(*) FROM genes WHERE introns=covered_introns AND introns > 0 AND splice_coverage >= 10",
-            'genes_partially_covered':
-                "SELECT count(*) FROM genes WHERE covered_introns > 0 AND covered_introns < introns AND introns > 0",
-            'genes_not_covered':
-                "SELECT count(*) FROM genes WHERE covered_introns = 0 AND introns > 0",
-            'introns':
-                "SELECT sum(introns) FROM genes",
-            'known_introns': 
-                "SELECT SUM(covered_introns) FROM genes",
-            'known_introns_%': 
-                "SELECT SUM(covered_introns)*1.0/sum(introns) FROM genes",
-            'known_introns_coverage': 
-                "SELECT sum(splice_coverage)*1.0/sum(covered_introns) FROM genes",
-#            'splices':
-#                "SELECT count(*) FROM splices",
-#            'genes_with_weak_splices':
-#                "SELECT count(*) FROM genes g LEFT JOIN splices s USING(gene) WHERE s.coverage < g.splice_coverage/100 AND s.coverage < 10",
-            }
-    names_order = [
-            'db',
-            'genes',
-            'genes_without_intron',
-            'genes_completely_covered',
-            'genes_completely_covered_10',
-            'genes_partially_covered',
-            'genes_not_covered',
-            'introns',
-#            'splices',
-            'known_introns',
-            'known_introns_%',
-            'known_introns_coverage',
-#            'genes_with_weak_splices'
+    queries = [
+            ['genes', "SELECT count(*) FROM genes"],
+            ['genes_without_intron', "SELECT count(*) FROM genes WHERE introns=0"],
+            ['genes_completely_covered', "SELECT count(*) FROM genes WHERE introns=covered_introns AND introns > 0"],
+#            ['genes_completely_covered_10', "SELECT count(*) FROM genes WHERE introns=covered_introns AND introns > 0 AND splice_coverage >= 10"],
+            ['genes_partially_covered', "SELECT count(*) FROM genes WHERE covered_introns > 0 AND covered_introns < introns AND introns > 0"],
+            ['genes_not_covered', "SELECT count(*) FROM genes WHERE covered_introns = 0 AND introns > 0"],
+            ['introns', "SELECT sum(introns) FROM genes"],
+            ['known_introns', "SELECT SUM(covered_introns) FROM genes"],
+            ['known_introns_%', "SELECT SUM(covered_introns)*1.0/sum(introns) FROM genes"],
+            ['known_introns_coverage', "SELECT sum(splice_coverage)*1.0/sum(covered_introns) FROM genes"],
+#            ['splices', "SELECT count(*) FROM splices"],
+#            ['genes_with_weak_splices', "SELECT count(*) FROM genes g LEFT JOIN splices s USING(gene) WHERE s.coverage < g.splice_coverage/100 AND s.coverage < 10"],
             ]
 
     def run(self):
@@ -76,19 +49,23 @@ class Stats(eHive.BaseRunnable):
         stats = {
                 'db': path
                 }
-        for name, query in Stats.queries.items():
+        for q in Stats.queries:
+            name = q[0]
+            query = q[1]
+            #query = query.replace('gene', 'transcript')
             stats[name] = input_db.query_count(query)
 
         return stats
     
     def print_stats(stats, output):
-
-        header = "#" + "\t".join(Stats.names_order)
+        
+        names_order = [q[0] for q in Stats.queries]
+        header = "#" + "\t".join(names_order)
         
         stat_lines = [header]
         for stat in stats:
             line = []
-            for name in Stats.names_order:
+            for name in names_order:
                 line.append(str(stat[name]))
             stat_line = "\t".join(line)
             stat_lines.append(stat_line)
